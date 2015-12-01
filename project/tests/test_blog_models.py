cd006780a534b539ace7c7dc4e26c7d4e1f950c6
Suraj Bennur <sbennur@outlook.com>
2015-12-01 14:30:54 -0800

    adding the unit tests in the main master branch

---
+++ project/tests/test_blog_models.py
@@ -0,0 +1,20 @@
+import unittest
+
+class TestListing(unittest.TestCase):
+    def test___str__(self):
+        # listing = Listing()
+        # self.assertEqual(expected, listing.__str__())
+        assert False # TODO: implement your test here
+
+    def test_get_absolute_url(self):
+        # listing = Listing()
+        # self.assertEqual(expected, listing.get_absolute_url())
+        assert False # TODO: implement your test here
+
+    def test_publish(self):
+        # listing = Listing()
+        # self.assertEqual(expected, listing.publish())
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()

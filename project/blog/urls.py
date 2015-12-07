507f54d28909b1cacca9e3c0edb32ab6bdf85ee4
Chris Tarazi <chris.tarazi@yahoo.com>
2015-12-07 12:32:23 -0800

    Added notifications feature; Users can now press a button to show interest in an item; Users have the ability to view who is interested in their items; Users can also view all their notifications and mark them as read; Using a new package called django-notificaitons-hq which I modified to fit our needs; you can find it stored in the root directory, called notifications. Please overwrite the env/lib/site-packages/notifications with that folder.

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -1,10 +1,12 @@
 ﻿from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 from .views import ListingUpdate, ListingDelete
+
+import notifications
 
 # Each url pattern is tied to a view.
 # If you check views.py, you'll notice that we are defining "base".
@@ -29,6 +31,12 @@ urlpatterns = [
 	url(r'^listing/update/(?P<listing_id>[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})$', ListingUpdate.as_view(), name='listing_update'),
 
 	url(r'^search$', views.search, name='search'),
+
+	# The following url patters are for the notifications feature.
+	url(r'^inbox/notifications/', include(notifications.urls), name='notifications'),
+	url(r'^inbox/notifications/mark-all-as-read$', views.mark_all_as_read, name='mark_all_as_read'),
+	url(r'^get-interested-users$', views.get_interested_users, name='get_interested_users'),
+	url(r'^send-notification$', views.send_notification, name='send_notification'),
 	
 	# Below is for the url of the pictures uploaded by users
 
--- project/mysite/settings.py
+++ project/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'notifications',
 )
 
 MIDDLEWARE_CLASSES = (
